07f99d534afb69868e975d451dd2f16a73db8851
Alice M. Yoon <misun6312@gmail.com>
2018-10-26 15:43:17 -0400

    scratch file updated

--- scratch.py
+++ scratch.py
@@ -1,6 +1,17 @@
 from itertools import combinations
 import json
 
+def make_dictionary(dict, source):
+	for word, v in source.items():
+		sorted_word = ''.join(sorted(word))
+		if sorted_word in dict:
+			dict[sorted_word][word] = v
+		else:
+			dict[sorted_word] = {}
+			dict[sorted_word][word] = v
+	return dict
+
+
 def get_possible_word(dict, jword):
 	if jword in dict:
 		return dict[jword]
@@ -12,17 +23,14 @@ def recur_search(dict, remain_chars, nch_list, res, score):
 		jword = ''.join(sorted(remain_chars))
 		pws = get_possible_word(dict,jword)
 		if pws != '':
-			most_frequent_word = max(pws,key=pws.get)
+			most_frequent_word = max(pws, key=pws.get)
 			v = pws[most_frequent_word]
 			res.append(most_frequent_word)
 			score.append(v)
-
-			# final_list[cnt] = res
-			# cnt += 1
-			final_list.append(' '.join(res))
-			total_score.append(sum(score))
-			# print res
-			# final_list[res] = sum(score)
+			final_cand = ' '.join(res)
+			if final_cand not in final_list:
+				final_list.append(final_cand)
+				total_score.append(sum(score))
 			if len(res)>1:
 				res.pop()
 				res.pop()
@@ -56,22 +64,17 @@ def recur_search(dict, remain_chars, nch_list, res, score):
 
 
 def main():
+
 	json_data=open('freq_dict.json').read()
-	d = json.loads(json_data)
+	data = json.loads(json_data)
+
 
 	dict = {}
-	for word, v in d.items():
-		sorted_word = ''.join(sorted(word))
-		# dict[sorted_word][word] = v
-		if sorted_word in dict:
-			dict[sorted_word][word] = v
-		else:
-			dict[sorted_word] = {}
-			dict[sorted_word][word] = v
+	make_dictionary(dict, data)
 
 	#find jumbles
-	inp = {'NAGLD':'01011','RAMOJ':'00110','CAMBLE':'110100','WRALEY':'101010'}
-	inp2 = [3,4,4]
+	# inp = {'NAGLD':'01011','RAMOJ':'00110','CAMBLE':'110100','WRALEY':'101010'}
+	# inp2 = [3,4,4]
 
 	# inp = {'BNEDL':'10001','IDOVA':'10011','SEHEYC':'010001','ARACEM':'010011'}
 	# inp2 = [3,4,3]
@@ -82,27 +85,21 @@ def main():
 	# inp = {'KNIDY':'11000','LEGIA':'10100','CRONEE':'010100','TUVEDO':'100001'}
 	# inp2 = [8]
 	
-	# inp = {'GYRINT':'110100','DRIVET':'001001','SNAMEA':'100001','CEEDIT':'010101','SOWDAH':'100100','ELCHEK':'010001'}
-	# inp2 = [6, 8]
+	inp = {'GYRINT':'110100','DRIVET':'001001','SNAMEA':'100001','CEEDIT':'010101','SOWDAH':'100100','ELCHEK':'010001'}
+	inp2 = [6, 8]
 
 	chars = []
 	for i, v in inp.items():
 		jword = ''.join(sorted(i.lower()))
 		pws = get_possible_word(dict,jword)
 		most_frequent_word = max(pws,key=pws.get)
-		print i, most_frequent_word
+		print i, '\t|', most_frequent_word, '\t<= ', pws.keys()
 
 		chars.extend([most_frequent_word[i] for i, x in enumerate(v) if x == "1"])
 
 	main_chars = ''.join(chars)
 
 	print main_chars
-
-	# perm = permutations('lwejolndbea',inp2[0])
-	# perm = permutations('lwejolndbea',inp2[1])
-	# perm = permutations('lwejolndbea',inp2[2])
-	# => total score => max score
-
 
 	global final_list
 	global total_score
@@ -116,9 +113,6 @@ def main():
 
 	idlist = sorted(range(len(total_score)), key=lambda k: total_score[k], reverse=True)
 	
-	# idd = total_score.index(max(total_score))
-	# print(max(total_score), final_list[idd])
-
 	maxN = 20
 	for i in idlist[0:maxN]:
 		print total_score[i], final_list[i]
